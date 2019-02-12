76345777675f47a25a000026ca0d39436e87a185
Ed Weston <edward.weston@newvoicemedia.com>
2019-02-12 12:41:12 +0000

    change name to tournaName avoid reserved word issues.

--- src/commandParser.js
+++ src/commandParser.js
@@ -34,18 +34,18 @@ const parseCurrent = (parameters, channelID) => {
 }
 
 const parseNew = (parameters) => {
-  let name = null
+  let tournaName = null
   let rounds = null
   try {
-    name = parameters[0]
+    tournaName = parameters[0]
     rounds = parameters[parameters.findIndex(param => param === 'rounds') - 1]
 
     if (!isNormalInteger(rounds) || rounds < 3) return { err: 'rounds not found or too small.' }
-    if (!/\S/.test(name)) return { err: 'name not found' }
+    if (!/\S/.test(tournaName)) return { err: 'name not found' }
   } catch (err) {
     return { error: 'new command invalid try: /tourneyBot new -n mytournament -r 4' }
   }
-  return { type: 'newTournament', data: { name, rounds } }
+  return { type: 'newTournament', data: { tournaName, rounds } }
 }
 
 const isNormalInteger = (str) => {
--- src/dataAccess/newTournament.js
+++ src/dataAccess/newTournament.js
@@ -4,12 +4,12 @@ const documentDB = new AWS.DynamoDB.DocumentClient()
 const execute = async (data) => {
   const params = {
     TableName: 'tournaBot-tournaments',
-    Item: { name: data.name, rounds: data.rounds }
+    Item: { tournaName: data.tournaName, rounds: data.rounds }
   }
 
   await documentDB.put(params).promise()
 
-  return `${data.name} tournament started, with ${data.rounds} rounds.`
+  return `${data.tournaName} tournament started, with ${data.rounds} rounds.`
 }
 
 module.exports = { execute }
