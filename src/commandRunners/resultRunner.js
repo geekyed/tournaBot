467d416c876c5392db6c4b85f4d9b8fca575c354
Ed Weston <edward.weston@newvoicemedia.com>
2019-02-25 16:01:01 +0000

    mark and report if the result finishs the round

--- src/commandRunners/resultRunner.js
+++ src/commandRunners/resultRunner.js
@@ -12,12 +12,14 @@ const execute = async (data) => {
     if (matches[i].player1.includes(data.userID)) {
       matches[i].score.player1 = data.score.user
       matches[i].score.player2 = data.score.opponent
+      matches[i].completed = true
       return await saveTournament(myTournament, data)
     }
     // Is user player 2 of this match?
     if (matches[i].player2.includes(data.userID)) {
       matches[i].score.player1 = data.score.opponent
       matches[i].score.player2 = data.score.user
+      matches[i].completed = true
       return await saveTournament(myTournament, data)
     }
   }
@@ -27,15 +29,28 @@ const execute = async (data) => {
 }
 
 const saveTournament = async (myTournament, data) => {
-  myTournament.rounds[myTournament.currentRound - 1].started = true
+  let rounds = myTournament.rounds[myTournament.currentRound - 1]
+  rounds.started = true
+  const isRoundFinished = allMatchesCompleted(rounds.matches)
+  if (isRoundFinished) myTournament.currentRound++
+
   await tournament.set(myTournament)
-  return createResponse(data.userID, data.score)
+  return createResponse(data.userID, data.score, isRoundFinished)
 }
 
-const createResponse = (user, score) => {
-  if (score.user > score.opponent) return `Result saved, congrats on the win <@${user}>!`
-  if (score.opponent > score.user) return `Result saved, better luck next time <@${user}>`
-  if (score.opponent === score.user) return `Result saved, oh you drew <@${user}>`
+const allMatchesCompleted = (matches) => {
+  matches.forEach(match => {
+    if (match.completed === false) return false
+  })
+  return true
+}
+
+const createResponse = (user, score, isRoundFinished) => {
+  let finished = ''
+  if (isRoundFinished) finished = ' The round has finished, use generate to create the next set of pairings'
+  if (score.user > score.opponent) response = `Result saved, congrats on the win <@${user}>!${finished}`
+  if (score.opponent > score.user) response = `Result saved, better luck next time <@${user}>${finished}`
+  if (score.opponent === score.user) response = `Result saved, oh you drew <@${user}>${finished}`
 }
 
 module.exports = { execute }
