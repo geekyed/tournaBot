cb1d149fc16eb0852bb68c5e485441d5f35782f4
Ed Weston <edward.weston@newvoicemedia.com>
2019-03-04 10:50:49 +0000

    stores results correctly, (not returning after first)

--- src/commandRunners/resultRunner.js
+++ src/commandRunners/resultRunner.js
@@ -7,18 +7,16 @@ const execute = async (data) => {
 
   let round = myTournament.rounds[myTournament.currentRound - 1]
 
-  let resultString = ''
   for (let i in round.matches) {
     let match = round.matches[i]
 
     if (match.player1.includes(data.userID)) {
-      resultString = processMatch(match, round, myTournament, data.score.user, data.score.opponent)
+      return processMatch(match, round, myTournament, data.score.user, data.score.opponent)
     }
     // Is user player 2 of this match?
     if (match.player2.includes(data.userID)) {
-      resultString = processMatch(match, round, myTournament, data.score.opponent, data.score.user)
+      return processMatch(match, round, myTournament, data.score.opponent, data.score.user)
     }
-    return resultString
   }
 
   throw new Error(`no current match found in tournament: ${myTournament.tournamentName}, 
@@ -27,8 +25,12 @@ const execute = async (data) => {
 
 const processMatch = async (match, round, myTournament, p1Score, p2Score) => {
   setScores(match, p1Score, p2Score)
+  console.log('scores set')
   administerRound(myTournament, round)
+  console.log('admin done')
+
   resultString = setPoints(match, round)
+  console.log('points set')
 
   await tournament.set(myTournament)
   return resultString
@@ -45,17 +47,19 @@ const setPoints = (match, round) => {
   if (match.score.player1 === match.score.player2) { 
     round.points[match.player1] = 1
     round.points[match.player2] = 1
-    return `Result saved, oh you drew <@${match.player1}>`
+    return `Result saved, oh you drew ${match.player1}`
   }
 
   if (match.score.player1 > match.score.player2) {
     round.points[match.player1] = 3
-    return `Result saved, congrats on the win <@${match.player1}>!`
+    round.points[match.player2] = 0
+    return `Result saved, congrats on the win ${match.player1}!`
   }
 
   if (match.score.player1 < match.score.player2) {
+    round.points[match.player1] = 0
     round.points[match.player2] = 3
-    return `Result saved, better luck next time <@${match.player1}>`
+    return `Result saved, better luck next time ${match.player1}`
   }
 
   throw new Error('There\'s something worng with the format of your score')
