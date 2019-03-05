1973081577f7e867ff87ddb6d82dc460a46623bb
Ed Weston <edward.weston@newvoicemedia.com>
2019-03-05 14:50:27 +0000

    refector generate matches

--- src/commandRunners/generateMatchesRunner.js
+++ src/commandRunners/generateMatchesRunner.js
@@ -10,35 +10,17 @@ const execute = async (data) => {
   if (myTournament.currentRound === 1) myTournament.rounds = []
   let round = { matches:[], started: false, points: {} }
 
-  let totalScores = []
-  for (let i in myTournament.players) {
-    round.points[myTournament.players[i]] = 0 // init this rounds points
-    let points = 0
-    for (let j in myTournament.rounds) { // get previous rounds points
-      points += myTournament.rounds[j].points[myTournament.players[i]]
-    }
-    totalScores.push({ name: myTournament.players[i], points })
-  }
-
-  totalScores.sort((a, b) => a.points - b.points)
+  let totalScores = collateTotalScore(myTournament, round)
+  totalScores.sort((a, b) => a.points - b.points) // Sort the scores ready for pairing.
 
   let matchesString = `Round ${myTournament.currentRound} matches generated!\n`
 
   while (totalScores.length !== 0) {
     const player1 = totalScores.pop()
     const player2 = totalScores.pop()
-    let newMatch = {
-      player1: player1.name,
-      player2: player2.name,
-      completed: this.player2 === 'Bye',
-      score: {
-        player1: 0,
-        player2: 0
-      }
-    }
-
+    
     matchesString += `${player1.name} ${player1.points}pts vs ${player2.name} ${player2.points}pts\n`
-    round.matches.push(newMatch)
+    round.matches.push(createMatch(player1, player2))
     if (player2 === 'Bye') round.points[player1] = 3
   }
 
@@ -48,6 +30,29 @@ const execute = async (data) => {
   return matchesString
 }
 
+const createMatch = (player1, player2) => {
+  return newMatch = {
+    player1: player1.name,
+    player2: player2.name,
+    completed: this.player2 === 'Bye',
+    score: {
+      player1: 0,
+      player2: 0
+    }
+  }
+}
 
+const collateTotalScore = (myTournament, round) => {
+  let totalScores = []
+  for (let i in myTournament.players) {
+    round.points[myTournament.players[i]] = 0 // init this rounds points
+    let points = 0
+    for (let j in myTournament.rounds) { // get previous rounds points
+      points += myTournament.rounds[j].points[myTournament.players[i]]
+    }
+    totalScores.push({ name: myTournament.players[i], points })
+  }
+  return totalScores
+}
 
 module.exports = { execute }
