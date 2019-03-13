d887e3e69b0a332f41d296c1c7b46389d1bd22e9
Ed Weston <edward.weston@newvoicemedia.com>
2019-03-13 21:31:10 +0000

    output points in point runner.

--- src/commandRunners/pointsRunner.js
+++ src/commandRunners/pointsRunner.js
@@ -1,12 +1,17 @@
 const tournament = require('../dataAccess/tournament')
 const currentTournament = require('../dataAccess/currentTournament')
+const { collateTotalScores } = require('./helpers/collateTotalScores')
 
 const execute = async (data) => {
   const tournamentChannelLink = await currentTournament.get(data.channelID)
 
   let myTournament = await tournament.get(tournamentChannelLink.tournamentName)
 
-  
-  return JSON.stringify(myTournament)
+  let totalScores = collateTotalScores(myTournament).sort((a, b) => b.points - a.points)
+
+  let pointsResponse = 'Current points standings:\n\n'
+  totalScores.forEach(score => pointsResponse += `${score.points}pts ${score.name}\n`)
+
+  return pointsResponse
 }
 module.exports = { execute }
