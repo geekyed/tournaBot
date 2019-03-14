1a3d2b89f7989f7000220f9914d796281624c0a1
Ed Weston <edward.weston@newvoicemedia.com>
2019-03-14 08:34:10 +0000

    round now calls generate for the next round.

--- src/commandRunners/roundRunner.js
+++ src/commandRunners/roundRunner.js
@@ -1,6 +1,7 @@
 const tournament = require('../dataAccess/tournament')
 const currentTournament = require('../dataAccess/currentTournament')
 const { determineTotalRounds } = require('./helpers/determineTotalRounds')
+const generateMatchesRunner = require('./generateMatchesRunner')
 
 const execute = async (data) => {
   const tournamentChannelLink = await currentTournament.get(data.channelID)
@@ -11,7 +12,9 @@ const execute = async (data) => {
   if (round.matches.every( m => m.completed ) && myTournament.currentRound < determineTotalRounds(myTournament.players.length) ) {
     myTournament.currentRound++
     await tournament.set(myTournament)
-    return `Starting round ${myTournament.currentRound}, you can now generate new pairings.`
+    let response = `Round ${myTournament.currentRound - 1} Ended.\n\n`
+    response += await generateMatchesRunner.execute(data)
+    return response
   }
   return 'Round not incremented.'
 }
