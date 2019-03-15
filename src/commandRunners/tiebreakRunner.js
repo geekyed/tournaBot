254aeb53373e4e02418b3a827c633f759f878f88
Ed Weston <edward.weston@newvoicemedia.com>
2019-03-15 11:04:13 +0000

    remove comment.

--- src/commandRunners/tiebreakRunner.js
+++ src/commandRunners/tiebreakRunner.js
@@ -1,7 +1,7 @@
 const execute = async () => {
   return 'If two players end a swiss tournament with equal points their position is determined by three tiebreak conditions, if one cannot find a winner we use the next rule.\n\n' +
   '   1. Opponents Match Win Percentage - this the the averge win percentage of matches your opponent has.\n' +
-  '   2. Your Game Win Percentage - of the games you played what percentage did you win. (a Match is made of 3 games in general)\n' +
+  '   2. Your Game Win Percentage - of the games you played what percentage did you win.\n' +
   '   3. Opponents Game Win Percentage - as above but an average of your opponents game win percentage.\n\n' +
   '  see https://www.wizards.com/dci/downloads/tiebreakers.pdf for more info.'
 }
