5728aeaf27b94fbec670b736c0904a681d73fcf6
Ed Weston <edward.weston@newvoicemedia.com>
2019-03-29 13:27:14 +0000

    fix scores if a round has been generated but we wish to add players

--- src/commandRunners/helpers/collateTotalScores.js
+++ src/commandRunners/helpers/collateTotalScores.js
@@ -11,24 +11,26 @@ const collateTotalScores = (tournament) => {
     points[name] = 0
     opponents[name] = []
 
-    tournament.rounds.forEach( round => {
-      points[name] += round.points[name]
-      let match = round.matches.find(match => match.player1 === name || match.player2 === name)
-      gamesPlayed += match.score.player1 + match.score.player2
-      if (match.player1 === name) {
-        gamePoints += (3 * match.score.player1)
-        if (match.player2 != 'Bye') opponents[name].push(match.player2)
-      }
-      if (match.player2 === name) {
-        gamePoints += (3 * match.score.player2)
-        if (match.player2 != 'Bye') opponents[name].push(match.player1)
-      }
-      //  Drawn last game assumed if 1-1
-      if (match.score.player1 === match.score.player2) {
-        gamePoints += 1
-        gamesPlayed += 1
-      }
-    })
+    if (tournament.rounds.length >= 1 && tournament.rounds[0].started) {
+      tournament.rounds.forEach( round => {
+        points[name] += round.points[name]
+        let match = round.matches.find(match => match.player1 === name || match.player2 === name)
+        gamesPlayed += match.score.player1 + match.score.player2
+        if (match.player1 === name) {
+          gamePoints += (3 * match.score.player1)
+          if (match.player2 != 'Bye') opponents[name].push(match.player2)
+        }
+        if (match.player2 === name) {
+          gamePoints += (3 * match.score.player2)
+          if (match.player2 != 'Bye') opponents[name].push(match.player1)
+        }
+        //  Drawn last game assumed if 1-1
+        if (match.score.player1 === match.score.player2) {
+          gamePoints += 1
+          gamesPlayed += 1
+        }
+      })
+    }
     // Lowest win %age is 33% to even out low scoring players - https://www.wizards.com/dci/downloads/tiebreakers.pdf
     matchWinPercentage[name] = Math.max((points[name] / (tournament.rounds.length * 3)), 0.33) * 100 
     gameWinPercentage[name] = (gamePoints / (gamesPlayed * 3)) * 100
