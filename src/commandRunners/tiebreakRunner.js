eae93b427b66024bb95a0793694879923918bd9f
Ed Weston <edward.weston@newvoicemedia.com>
2019-04-04 14:48:40 +0100

    fix weird spacing.

--- src/commandRunners/tiebreakRunner.js
+++ src/commandRunners/tiebreakRunner.js
@@ -1,5 +1,6 @@
 const execute = async () => {
-  return { header: 'Tiebreakers', message: 'If two players end a swiss tournament with equal points their position is' + 'determined by three tiebreak conditions, if one cannot find a winner we use the next rule.\n' +
+  return { header: 'Tiebreakers', message: 'If two players end a swiss tournament with equal points their position is' + 
+  ' determined by three tiebreak conditions, if one cannot find a winner we use the next rule.\n' +
   '   1. Opponents Match Win Percentage - this the the averge win percentage of matches your opponent has.\n' +
   '   2. Your Game Win Percentage - of the games you played what percentage did you win.\n' +
   '   3. Opponents Game Win Percentage - as above but an average of your opponents game win percentage.\n\n' +
